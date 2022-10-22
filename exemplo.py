179039d8141f571a1c1ed862521ec49083ea2ed0
amoraes2000 <amoraes2000@gmail.com>
2022-10-22 10:54:21 -0300

    up

--- exemplo.py
+++ exemplo.py
@@ -4,6 +4,17 @@
 #
 # data: 22/10/2022
 
+# 5. Desenvolva um programa em linguagem Python que recebe do
+# usuário o preço de um produto e um valor de desconto. Com  
+# estas informações, o programa deve exibir o preço do       
+# produto com desconto. Observações: O valor do desconto deve
+# ser informado entre 1 (100%) e 0 (0%). Assim, 0.5          
+# corresponde a um desconto de 50%. Neste momento, considere 
+# que sempre serão informados valores válidos pelo usuário.
+# caso precise formatar o valor de um número numérico,       
+# sugerimos que assistam a este vídeo: link vídeo do Youtube.
+# https://youtu.be/7QuriwzICaY
+
 # Entrada de dados
 
 preco = float(input("informe o preço do produto: ")) # Solicita o preço do produto
